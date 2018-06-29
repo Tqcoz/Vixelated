cdd4ad2f497c5556821e80f7e5478cfd1bae6ca5
Tqcoz <33169104+Tqcoz@users.noreply.github.com>
2018-06-28 22:40:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ bot.on("message", async message => {
 
   if (cmd === `${prefix}help`){
     message.channel.send("**Hello, I am Vixelated! I was actually an Human like you until I got bit by an Vixel which is a female fox and then I turned into an Vixel, after that a Tqco named Tqco_Tuesdqy found me and named me Vixelated. I now work for Vixelated which is a Minecraft Client and Web Browser, I am making for people. Sadly it is not available yet due to the fact it's not finished. Whenever it is, I will be sure to announce it to you guys!**");
-});  
+  }
   if (cmd === `${prefix}`){
     message.channel.send("**Yes? You ruined my time blowing bubbles with Vixels. Anyway, what do you need? I will be sure to tell Tqco_Tuesdqy to answer you soon as possible!**");
-}); 
+  }
  const DiscordRPC = require("discord-rpc");
 
 const ClientId = '462054174234902541';
